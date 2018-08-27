55bedb7a8c4ea160d0206337b0a883cf0bec0977
bernardoabreu <bernardoaabreu@gmail.com>
2018-08-27 17:32:46 -0300

    Update run script to create output folder

--- run.sh
+++ run.sh
@@ -6,19 +6,7 @@ INPUT_DIR="${BASE}/input_files"
 OUTPUT_DIR="${BASE}/output"
 
 
-# # parse the options
-# while getopts 'd:v:h' opt ; do
-#   case $opt in
-#     d) DATA_SETS=("$OPTARG");;
-#     v) TEST_VERSION=$OPTARG; USE_ALL=false ;;
-#     h) echo "Options: -d data sets (default: ${DATA_SETS[*]})"
-#        echo "         -v test version (default: Use all)";
-#        exit 0 ;;
-#   esac
-# done
-
-# # skip over the processed options
-# shift $((OPTIND-1)) 
+mkdir -p $OUTPUT_DIR
 
 
 ls $INPUT_DIR | while read -r FILE ; do
